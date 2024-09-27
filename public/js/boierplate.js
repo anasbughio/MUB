09cfba5e3947d706928ae130b35a8524367bff47
anasbughio <112558242+anasbughio@users.noreply.github.com>
2024-09-27 22:58:07 +0500

    Update boierplate.js

--- public/js/boierplate.js
+++ public/js/boierplate.js
@@ -3,11 +3,11 @@
 let pathToIncludes = '';
 
 // If the current HTML file is inside 'views/pages/', go up two directories
-if (window.location.pathname.includes('/views/pages/')) {
-  pathToIncludes = '/views/includes/';
+if (window.location.pathname.includes('../../views/pages/')) {
+  pathToIncludes = '../../views/includes/';
 } else {
   // For files in the root or other locations, keep the default path
-  pathToIncludes = '/views/includes/';
+  pathToIncludes = '../../views/includes/';
 }
 
 // Load navbar
